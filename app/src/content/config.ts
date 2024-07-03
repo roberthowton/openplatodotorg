36f5dd77c7f3a32b8a5b424826597ecbef3d9613
Nathan Meyer <Canon44444@gmail.com>
2024-07-03 00:19:11 -0400

    Reshuffle Content Collection paths

--- app/src/content/config.ts
+++ app/src/content/config.ts
@@ -3,7 +3,7 @@ import { z, defineCollection } from 'astro:content';
 // 2. Define your collection(s)
 const dialogCollection = defineCollection({
     type: "data",
-    schema: z.record(z.string().regex(/\d{3}/), z.record(z.enum(['a', 'b', 'c', 'd', 'e']), z.string()))
+    schema: z.record(z.string().regex(/\d+/), z.record(z.enum(['a', 'b', 'c', 'd', 'e']), z.string()))
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
