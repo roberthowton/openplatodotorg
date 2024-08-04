339604a8bdada6d66fbcccd12478b05e64530760
Robert Howton <robert.f.howton@gmail.com>
2024-08-04 16:20:15 -0400

    create dialogV2 schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,18 @@ const dialogCollection = defineCollection({
   ),
 });
 
+const dialogV2Collection = defineCollection({
+  type: "data",
+  schema: z.record(
+    z.string().regex(/\d{3}/),
+    z.record(
+      z.enum(["a", "b", "c", "d", "e"]),
+      z.record(z.string().regex(/\d{1}/), z.string()),
+    ),
+  ),
+});
+
 export const collections = {
   dialogs: dialogCollection,
+  dialogV2: dialogV2Collection,
 };
