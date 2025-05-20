c414b63c784c5ec272cf0c8bb3d7800b23a8bea3
robert howton <robert.f.howton@gmail.com>
2025-05-20 13:52:21 -0400

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://openplato.org",
   integrations: [react()],
   output: "server",
+  adapter: vercel(),
 });
