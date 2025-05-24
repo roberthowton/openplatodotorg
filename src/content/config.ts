fdfda014c8b7b6a7c61c28617b5b247731f4c2b6
robert howton <robert.f.howton@gmail.com>
2025-05-24 16:30:09 -0400

    reorg content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,19 @@
+import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 
-const dialogCollection = defineCollection({
-  type: "data",
-  schema: z.record(
-    z.string().regex(/\d+/),
-    z.record(z.enum(["a", "b", "c", "d", "e"]), z.string())
-  ),
+// const dialogCollection = defineCollection({
+//   type: "data",
+//   schema: z.record(
+//     z.string().regex(/\d+/),
+//     z.record(z.enum(["a", "b", "c", "d", "e"]), z.string()),
+//   ),
+// });
+
+const dialogue = defineCollection({
+  loader: glob({ pattern: "**/*.xml", base: "./src/content/dialogue" }),
 });
 
 export const collections = {
-  dialogs: dialogCollection,
+  // dialogs: dialogCollection,
+  dialogue,
 };
