5c375867864743d7b8d8b21bbe159e2810285dca
robert howton <robert.f.howton@gmail.com>
2025-05-25 19:03:17 -0400

    remove nano state

--- src/stores/show-state.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'nanostores'
-import { ShowState } from '../types';
-
-export const $showState = atom<ShowState>(ShowState.BOTH)
-
-export function setShow(newShowState: ShowState) {
-  $showState.set(newShowState);
-}
