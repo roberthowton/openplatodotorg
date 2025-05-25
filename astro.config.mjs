3c467cfc371335c38920702c3c2365389fd6bd4f
robert howton <robert.f.howton@gmail.com>
2025-05-25 19:07:41 -0400

    remove react and nanostores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://openplato.org",
-  integrations: [react()],
   output: "server",
   adapter: vercel(),
 });
