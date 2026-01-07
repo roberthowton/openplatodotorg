d8d94382bc3e6e0ff09f29fab8564f4fab598493
robert howton <robert.f.howton@gmail.com>
2026-01-07 10:58:19 -0500

    ad modules

--- src/module.d.ts
+++ src/module.d.ts
@@ -1,1 +1,21 @@
 declare module "CETEIcean";
+
+declare global {
+  namespace astroHTML.JSX {
+    interface IntrinsicElements {
+      "show-button": {
+        "data-show"?: string;
+        class?: string;
+        children?: any;
+      };
+      "tei-container": {
+        "set:html"?: string;
+        "data-root-id"?: string;
+        "data-usebehaviors"?: string;
+        "data-elements"?: string;
+        style?: string;
+        [key: string]: any;
+      };
+    }
+  }
+}
