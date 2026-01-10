a694560f55a781ace9d726ef76a72ed935b85f3a
robert howton <robert.f.howton@gmail.com>
2026-01-10 12:30:43 -0500

    add coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'happy-dom',
+    environment: "happy-dom",
     globals: true,
-    include: ['**/__tests__/**/*.test.ts'],
+    include: ["**/__tests__/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+    },
   },
 });
